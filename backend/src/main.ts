1b3128a2bcffc01141b01b7866edfde7243f6ad0
Gabrigas <artroxgabriel@gmail.com>
2025-12-24 08:10:14 -0300

    feat: add openapi and mongoose

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { apiReference } from '@scalar/nestjs-api-reference';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('Weather API')
+    .setDescription('The Weather API description')
+    .setVersion('1.0')
+    .addTag('weather')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  app.use(
+    '/api/docs',
+    apiReference({
+      content: documentFactory,
+    }),
+  );
+
+  await app.listen(process.env.NESTJS_PORT ?? 3000);
 }
-bootstrap();
+bootstrap().catch((err) => console.error(err));
