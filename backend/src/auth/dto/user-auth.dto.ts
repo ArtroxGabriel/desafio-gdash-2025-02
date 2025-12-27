c2890f86c37eb1d43c481000e7da25f9e600b325
Gabrigas <artroxgabriel@gmail.com>
2025-12-27 15:40:30 -0300

    docs: add api property to auth dtos

--- backend/src/auth/dto/user-auth.dto.ts
+++ backend/src/auth/dto/user-auth.dto.ts
@@ -1,19 +1,21 @@
-import { UserDto } from '../../user/dto/user.dto';
-import { User } from '../../user/schemas/user.schema';
 import { IsNotEmptyObject, ValidateNested } from 'class-validator';
+import { UserDto } from '../../user/dto/user.dto';
 import { UserTokensDto } from './user-tokens.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UserAuthDto {
   @ValidateNested()
   @IsNotEmptyObject()
+  @ApiProperty()
   readonly user: UserDto;
 
   @ValidateNested()
   @IsNotEmptyObject()
+  @ApiProperty()
   readonly tokens: UserTokensDto;
 
-  constructor(user: User, tokens: UserTokensDto) {
-    this.user = new UserDto(user);
+  constructor(user: UserDto, tokens: UserTokensDto) {
+    this.user = user;
     this.tokens = tokens;
   }
 }
--- backend/src/auth/dto/user-tokens.dto.ts
+++ backend/src/auth/dto/user-tokens.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty } from 'class-validator';
 
 export class UserTokensDto {
   @IsNotEmpty()
+  @ApiProperty()
   readonly accessToken: string;
 
   @IsNotEmpty()
+  @ApiProperty()
   readonly refreshToken: string;
 
   constructor(tokens: UserTokensDto) {
